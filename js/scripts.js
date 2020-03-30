83f287134c23e5e2b5b612eb8b00dbfc910aad4f
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-03-30 10:15:17 -0700

    add variables to js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-  $("formOne").submit(function() {
-    
+  $("#formOne").submit(function(event) {
+    var movie = $("input#movie").val();
+    var food = $("input#food").val();
+    var drink = $("input#drink").val();
+    var show = $("input#tv-show").val();
+    var activity = $("input#activity").val();
+    console.log(movie);
+    // var things = 
     event.preventDefault();
     
-  })
-})
+  });
+});
